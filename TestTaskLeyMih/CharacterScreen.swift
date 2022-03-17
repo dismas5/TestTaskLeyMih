2b19ce4e736e692c7e9490990a3b3948f5420365
Chornohub <chornohub@github.com>
2022-03-17 22:38:21 +0200

    Added more information to second screen

--- TestTaskLeyMih/CharacterScreen.swift
+++ TestTaskLeyMih/CharacterScreen.swift
@@ -10,23 +10,31 @@ class CharacterScreen: UIViewController {
     @IBOutlet weak var speciesLabel: UILabel!
     @IBOutlet weak var typeLabel: UILabel!
     @IBOutlet weak var genderLabel: UILabel!
+    @IBOutlet weak var episodesLabel: UILabel!
     @IBOutlet weak var createdLabel: UILabel!
+    @IBOutlet weak var expandButton: UIButton!
     
     var character: Character?
     var image: UIImage?
     
     override func viewDidLoad() {
-        if character != nil {
+        if let character = character {
             if let image = image {
                 characterPicture.image = image
             }
-            characterName.text = character?.name
-            idLabel.text! += String(character!.id)
-            statusLabel.text! += String(character!.status)
-            speciesLabel.text! += String(character!.species)
-            typeLabel.text! += String(character!.type)
-            genderLabel.text! += String(character!.gender)
-            createdLabel.text! += String(character!.created)
+            characterName.text = character.name
+            idLabel.text! += String(character.id)
+            statusLabel.text! += character.status
+            speciesLabel.text! += character.species
+            typeLabel.text! += character.type
+            genderLabel.text! += character.gender
+            
+            expandButton.addTarget(self, action: #selector(expandEpisodes), for: .touchDown)
+            for episode in character.episode {
+                episodesLabel.text! += "\n\(episode)"
+            }
+            
+            createdLabel.text! += character.created
 
 //            print("List of episodes:")
 //            for episode in character.episode {
@@ -37,4 +45,13 @@ class CharacterScreen: UIViewController {
         
     }
     
+    @objc func expandEpisodes() {
+        if expandButton.titleLabel!.text! == "(show)" {
+            episodesLabel.numberOfLines = 0
+            expandButton.setTitle("(hide)", for: .normal)
+            return
+        }
+        episodesLabel.numberOfLines = 1
+        expandButton.setTitle("(show)", for: .normal)
+    }
 }
